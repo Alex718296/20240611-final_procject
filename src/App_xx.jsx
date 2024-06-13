a80614c6a3e09f242c30aa6b298f82fcb764a6ce
Alex718296 <as71829696@gmil.com>
2024-06-13 22:47:00 +0800

    Vercel page can't open right ->try02

--- src/App_xx.jsx
+++ src/App_xx.jsx
@@ -55,7 +55,7 @@ const router = createBrowserRouter([
       index : true,
       element: <Popular/>    
     },
-    {path: '/popular',
+    {path: '/Popular',
       element:<Popular/> 
     },
     {path: '/Latest',
